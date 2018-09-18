df17f32f5bf4ec398ea1f781f74d7b16d7a8672e
alimilhim <alimilhim5@gmail.com>
2018-09-19 00:37:41 +0300

    setup heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port=process.env.PORT || 3000;
 var app=express();
 
 
@@ -53,4 +54,6 @@ app.get('/bad', (req,res)=>{
     res.send({error:"error loading data "});
 });
 
-app.listen(3000);
+app.listen(port, ()=>{
+    console.log('listening to port ' +port );
+});
